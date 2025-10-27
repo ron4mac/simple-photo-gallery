17ccc2ec858f1aa3647d23bf98a212838aa58e0e
ron4mac <ron@rjconline.net>
2025-10-27 20:54:24 +0000

    don't allow build if requested directory (file) already exists

--- build.php
+++ build.php
@@ -1,45 +1,59 @@
 <?php
 $msg = '';
+$alert = '';
 $ask = 'true';
 if (isset($_POST['galloc'])) {
-	$ask = 'false';
-	$droot = $_SERVER['DOCUMENT_ROOT'].'/';
-	$gbase = $droot.$_POST['galloc'];
-	mkdir($gbase, 0777, true);
-	$gbase .= '/';
-	$cfg = (object) [
-		'thms' => (object) ['w'=>240,'h'=>180, 'q'=>90],
-		'flds' => (object) ['w'=>128,'h'=>128],
-		'css' => 'dark',
-		'title' => $_POST['galnam'],
-		'desc' => 'A new gallery built with the gallery builder',
-		'auth_users' => (object) [$_POST['admnam']=>password_hash($_POST['admpass'], null)]
-	];
-
-	// figure out this base location relative to root
-	$docr = $_SERVER['DOCUMENT_ROOT'];
-	$rloc = str_replace($docr, '', __DIR__);
-	if (strlen($rloc)==strlen(__DIR__)) {
-		// it's one of THOSE servers .. try to figure it out
-		$tmp = $docr;
-		while (strpos($rloc,basename($tmp))) {
-			$tmp = dirname($tmp);
+	while (true) {
+		$ask = 'false';
+		$droot = $_SERVER['DOCUMENT_ROOT'].'/';
+		$gbase = $droot.$_POST['galloc'];
+		if (file_exists($gbase)) {
+			$ask = 'true';
+			$alert = "Directory '{$_POST['galloc']}' already exists.";
+			break;
 		}
-		$docr = str_replace($tmp, '', $docr);
-		$rloc = substr(strstr($rloc,$docr), strlen($docr)+1);
+		mkdir($gbase, 0777, true);
+		$gbase .= '/';
+		$cfg = (object) [
+			'thms' => (object) ['w'=>240,'h'=>180, 'q'=>90],
+			'flds' => (object) ['w'=>128,'h'=>128],
+			'css' => 'dark',
+			'title' => $_POST['galnam'],
+			'desc' => 'A new gallery built with the gallery builder',
+			'auth_users' => (object) [$_POST['admnam']=>password_hash($_POST['admpass'], null)]
+		];
+	
+		// figure out this base location relative to root
+		$docr = $_SERVER['DOCUMENT_ROOT'];
+		$rloc = str_replace($docr, '', __DIR__);
+		if (strlen($rloc)==strlen(__DIR__)) {
+			// it's one of THOSE servers .. try to figure it out
+			$tmp = $docr;
+			while (strpos($rloc,basename($tmp))) {
+				$tmp = dirname($tmp);
+			}
+			$docr = str_replace($tmp, '', $docr);
+			$rloc = substr(strstr($rloc,$docr), strlen($docr)+1);
+		}
+	
+		file_put_contents($gbase.'config.json', json_encode($cfg, JSON_PRETTY_PRINT));
+		$data = file_get_contents(__FILE__, false, NULL, __COMPILER_HALT_OFFSET__);
+		$ifiles = explode('&&&&',$data);
+		foreach ($ifiles as $f) {
+			list($fp, $fd) = explode('====',$f);
+			$fd = str_replace('####', $rloc, $fd);
+			if (dirname($fp) !== '.') mkdir($gbase.dirname($fp), 0777, true);
+			file_put_contents($gbase.$fp, $fd);
+		}
+		mkdir($gbase.'media', 0777);
+		$msg .= '<h4><a href="../'.$_POST['galloc'].'/admin">Go there</a></h4>';
+		break;
 	}
+}
 
-	file_put_contents($gbase.'config.json', json_encode($cfg, JSON_PRETTY_PRINT));
-	$data = file_get_contents(__FILE__, false, NULL, __COMPILER_HALT_OFFSET__);
-	$ifiles = explode('&&&&',$data);
-	foreach ($ifiles as $f) {
-		list($fp, $fd) = explode('====',$f);
-		$fd = str_replace('####', $rloc, $fd);
-		if (dirname($fp) !== '.') mkdir($gbase.dirname($fp), 0777, true);
-		file_put_contents($gbase.$fp, $fd);
-	}
-	mkdir($gbase.'media', 0777);
-	$msg .= '<h4><a href="../'.$_POST['galloc'].'/admin">Go there</a></h4>';
+function pVal ($n)
+{
+	return isset($_POST[$n]) ? htmlspecialchars($_POST[$n]) : '';
 }
 ?>
 <!DOCTYPE html>
@@ -75,33 +89,36 @@ input[type="text"] {
 </style>
 <script>
 const ask = <?=$ask?>;
+const alrt = "<?=$alert?>";
+//if (alrt) alert(alrt);
 if (ask) {
 	document.addEventListener('DOMContentLoaded', function() {
 		const dlg = document.getElementById('newGdlg');
 		dlg.showModal();
+		if (alrt) setTimeout(alert(alrt), 2500);
 	});
 }
 </script>
 </head>
-<body>
+<body style="background-color:gray">
 <?=$msg?>
 <dialog id="newGdlg">
 	<form action="" method="POST">
 		<label>
 			Gallery Name:<br>
-			<input type="text" class="textin" name="galnam" required autofocus>
+			<input type="text" class="textin" name="galnam" value="<?=pVal('galnam')?>" required autofocus>
 		</label>
 		<label>
 			Server Location:<br>
-			<input type="text" class="textin" name="galloc" required autofocus>
+			<input type="text" class="textin" name="galloc" value="<?=pVal('galloc')?>" required>
 		</label>
 		<label>
 			Admin User Name:<br>
-			<input type="text" class="textin" name="admnam" required autofocus>
+			<input type="text" class="textin" name="admnam" value="<?=pVal('admnam')?>" required>
 		</label>
 		<label>
 			Admin Password:<br>
-			<input type="text" class="textin" name="admpass" required autofocus>
+			<input type="text" class="textin" name="admpass" value="<?=pVal('admpass')?>" required>
 		</label>
 		<div class="dbuts">
 			<input type="submit" value="Create">
