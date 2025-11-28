8e95a1b5cdc950990b94371983d944c01b6accf4
ron4mac <ron@rjconline.net>
2025-11-28 19:15:24 +0000

    allow authentication from other browsers

--- build/index.php
+++ build/index.php
@@ -11,7 +11,36 @@ if (!file_exists('.auth.php')) {
 		file_put_contents('.auth.php', '<?php $ckid = "'.$ckid.'"; ?>');
 		setcookie($ckid, 1, 0, '/gallery/');
 	}
+} else {
+	include '.auth.php';
+	if (isset($_COOKIE[$ckid])) {
+		header('Location: /gallery/build.php', true, 301);
+		exit;
+	} elseif (isset($_POST['unam'])) {
+		$ck = md5($_POST['unam']."\t".$_POST['pass']);
+		if ($ck == $ckid) {
+			setcookie($ckid, 1, 0, '/gallery/');
+			header('Location: /gallery/build.php', true, 301);
+			exit;
+		}
+	}
 }
-//} else {
-	header('Location: /gallery/build.php', true, 301);
-//}
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Gallery Builder</title>
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<style>input {font-size:large}</style>
+	</head>
+	<body>
+		<div class="path">
+			<form action="" method="post" style="margin-top:5rem;text-align:center">
+				<input name="unam" value="" placeholder="Username" required autofocus>
+				<input type="password" name="pass" value="" placeholder="Password" required>
+				<input type="submit" value="Authenticate">
+			</form>
+		</div>
+	</body>
+</html>
